78651e5333372958125771dd3fc69961b8ffc5e7
Skye Jacobs <skyejacobs@casema.nl>
2025-09-10 11:51:03 +0200

    Tekstuele opvulling van index

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ session_start();
 </head>
 <body>
 <nav>
+    <img src="images/logo.png" alt="logo electroworld">
+    <a href="">Categorieën</a>
     <?php if (isset($_SESSION['Login'])) { ?>
         <a href="profile.php">Profiel</a>
     <?php } ?>
@@ -25,10 +27,32 @@ session_start();
     <?php } ?>
 </nav>
 <header>
-
+    <h1>Electroworld</h1>
+    <p>Uw speciaalzaak in Electronica</p>
 </header>
 <main>
-    
+    <section>
+        <h2>Huishouden</h2>
+        <article>
+            <img src="images/laundrylion.webp" alt="Laundry lion">
+            <h3>Wasmachines</h3>
+        </article>
+        <article>
+            <img src="images/clean_square_-_b2s_offer_henry_wash_50_off_1.png" alt="Henry stofzuiger">
+            <h3>stofzuigers</h3>
+        </article>
+    </section>
+    <section>
+        <h2>Computer</h2>
+        <article>
+            <img src="images/seniorentablet.png" alt="Seniorentablet">
+            <h3>Tablets</h3>
+        </article>
+        <article>
+            <img src="images/01.-MSI-Vector-16-HX-A14VHG-671NL.png" alt="MSI laptop">
+            <h3>Laptops</h3>
+        </article>
+    </section>
 </main>
 <footer>
 
